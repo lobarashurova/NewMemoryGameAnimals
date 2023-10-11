883f5bee973d5e47a9bd12ae61890da5322b3f44
lobarashurova <106048869+lobarashurova@users.noreply.github.com>
2023-10-11 11:51:04 +0500

    ready to play market

--- app/src/main/java/uz/mlsoft/newmemorygameanimals/presentation/dialogs/WindDialog.kt
+++ app/src/main/java/uz/mlsoft/newmemorygameanimals/presentation/dialogs/WindDialog.kt
@@ -2,25 +2,19 @@ package uz.mlsoft.newmemorygameanimals.presentation.dialogs
 
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.mlsoft.newmemorygameanimals.R
 import uz.mlsoft.newmemorygameanimals.databinding.DialogWinBinding
 
-class WindDialog :DialogFragment(R.layout.dialog_win) {
-    private val binding by viewBinding (DialogWinBinding::bind)
+class WindDialog : DialogFragment(R.layout.dialog_win) {
+    private val binding by viewBinding(DialogWinBinding::bind)
     private lateinit var mediaPlayer: MediaPlayer
-    private lateinit var restartListener :(()->Unit)
+    private lateinit var restartListener: (() -> Unit)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         mediaPlayer = MediaPlayer.create(requireContext(), R.raw.u_click)
-
-//        binding.restartBtn.setOnClickListener {
-//            restartListener.invoke()
-//        }
         binding.menuBtn.setOnClickListener {
             mediaPlayer.start()
             findNavController().navigate(R.id.levelsScreen)
@@ -28,9 +22,6 @@ class WindDialog :DialogFragment(R.layout.dialog_win) {
         }
     }
 
-//    fun setRestartListener(block:()->Unit){
-//        this.restartListener=block
-//    }
 
     override fun onResume() {
         super.onResume()
